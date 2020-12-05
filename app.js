6d6dc32efb686743da56161f2d53203703940291
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-12-05 17:27:26 +0100

    Events learning

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-let modul = require('./module1');
 const util = require('util');
+const colors = require('colors');
 
-console.log(util.is(123));
+
+
+const noel = new Date('December 25, 2020 15:00:00')
+
+console.log(util.types.isDate(123).toString().red);
+console.log(util.types.isDate(noel).toString().green);
+
+console.log('Writing by Bruno HUDBERT'.rainbow);
+
+
+// class MyEmmitter {
+//     constructor(){
+//         this.events={
+//
+//
+//         }
+//     }
+//
+//     on(event,listener){
+//         this.events[event]=this.events[event] || []
+//         this.events[event].push(listener);
+//     }
+//     emit(event){
+//         if(this.events[event]){
+//             this.events[event].forEach( (listener) =>  listener() )
+//         }
+//     }
+// }
+const MyEmmitter = require('events');
+const emitter = new MyEmmitter();
+
+emitter.on('FILE_READ',()=>{
+    console.log('file wad read');
+})
+
+
+emitter.on('FILE_READ',()=>{
+    console.log('should save file');
+})
+
+emitter.emit('FILE_READ')
---
+++ events.js
@@ -0,0 +1,5 @@
+module.exports = {
+    FILE_OPENED : 'fileopened',
+    WRITTING_FILE : 'filewritting',
+    FILE_CLOSED : 'fileclosed'
+}
